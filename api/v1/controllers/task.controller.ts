351af1945d46a274585a6abb014c8a4c83a18ff8
K1EL2002 <94553932+ntkien021411@users.noreply.github.com>
2024-02-14 19:35:13 +0700

    Xóa công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -189,27 +189,27 @@ export const edit = async (req:Request, res:Response)  => {
   }
 };
 
-// //[DELETE] /api/v1/tasks/delete/:id
-// module.exports.delete = async (req:Request, res:Response)  => {
-//   try {
-//     const id = req.params.id;
-//     await Task.updateOne(
-//       {
-//         _id: id,
-//       },
-//       {
-//         deleted: true,
-//         deletedAt: new Date(),
-//       }
-//     );
-//     res.json({
-//       code: 200,
-//       message: "Xóa thành công!",
-//     });
-//   } catch (error) {
-//     res.json({
-//       code: 400,
-//       message: "Không tồn tại!",
-//     });
-//   }
-// };
+//[DELETE] /api/v1/tasks/delete/:id
+export const deleted = async (req:Request, res:Response)  => {
+  try {
+    const id :string = req.params.id;
+    await Task.updateOne(
+      {
+        _id: id,
+      },
+      {
+        deleted: true,
+        deletedAt: new Date(),
+      }
+    );
+    res.json({
+      code: 200,
+      message: "Xóa thành công!",
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Không tồn tại!",
+    });
+  }
+};
--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -15,5 +15,5 @@ router.post("/create", controller.create);
 
 router.patch("/edit/:id", controller.edit);
 
-// router.delete("/delete/:id", controller.delete);
+router.delete("/delete/:id", controller.deleted);
 export const taskRoutes:Router =  router;
